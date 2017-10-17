3b821277fcb2018c997d274b72c12eb00e8e7f18
Island0214 <549378813@qq.com>
2017-10-17 20:24:54 +0800

    <feat> : <增加主页面>

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -19,7 +19,7 @@
               <div class="left-wrapper">
                 <div class="inner-border"></div>
                 <h1>ISNOTE</h1>
-                <h2 style="top: 40%; left: 10%">在这里 <br><br> 一直还有你的样子 <br><br> 被保留。</h2>
+                <h2 style="top: 40%; left: 10%">在这里 <br><br> 一直还有你的笔记 <br><br> 被保留。</h2>
                 <!--我们同生 我们共享 无法离开 在云端之上-->
                 <!--一个人的伤怀 怎好意思感慨-->
                 <!--一直还有你的样子 被保留-->
@@ -43,17 +43,35 @@
     <div class="bottom-wrapper">
       <el-row :gutter="12" style="margin: 0;">
         <el-col :xs="24" :sm="8" :md="8" :lg="10" style="padding: 0">
-          <div class="card"></div>
+          <div class="card">
+            <h2 style="left: 10%; top: 2%;" class="contents">岛<br><br>屿<br><br>笔<br><br>记</h2>
+            <h4 style="left: 30%; top: -10%;">我<br><br>们<br><br>同<br><br>生</h4>
+            <h4 style="left: 50%; top: -10%;">我<br><br>们<br><br>共<br><br>享</h4>
+            <h4 style="left: 70%; top: -10%;">无<br><br>法<br><br>离<br><br>开</h4>
+            <h4 style="left: 90%; top: -10%;">在<br><br>云<br><br>端<br><br>之<br><br>上</h4>
+          </div>
         </el-col>
-        <el-col :xs="24" :sm="8" :md="8" :lg="4" style="padding: 0">
+        <el-col :xs="8" :sm="0" :md="0" :lg="0" style="padding: 0">
+          <div class="card">
+          </div>
+        </el-col>
+        <el-col :xs="8" :sm="8" :md="8" :lg="4" style="padding: 0">
           <div class="card">
             <img width="100%" src="../../assets/logo.png">
           </div>
         </el-col>
+        <el-col :xs="8" :sm="0" :md="0" :lg="0" style="padding: 0">
+          <div class="card">
+          </div>
+        </el-col>
         <el-col :xs="24" :sm="8" :md="8" :lg="10" style="padding: 0">
-          <div class="card"></div>
+          <div class="card">
+          </div>
         </el-col>
       </el-row>
+    </div>
+    <div class="contact-wrapper">
+      <h5 style="position: absolute; right: 10%; bottom: -10%; text-align: right">Create By @ISLAND<br>Email: 151250118@smail.nju.edu.cn</h5>
     </div>
   </div>
 </template>
